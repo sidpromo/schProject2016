1bb793927db104ef38fcf9d2221e58f0d99809db
gravitydrummer <gravitydrummer@gmail.com>
2016-08-25 09:24:36 +0200

    Hozzáadtam egy Console Application-t próbaképpen. Ki lehet törölni.

---
+++ ConsoleApplication_Temp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication_Temp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Ez csak egy teszt
+        }
+    }
+}
