4736daa974eb62749beba487f8d89cb6eabf791d
gravitydrummer <gravitydrummer@gmail.com>
2016-08-28 23:25:18 +0200

    Using, IF logika átalakítva, hangjelzés a hibánál. Using blokkba tettem az EducationDatabaseEntities példányosítását. Erre azért van szükség, mert így a kódblokk lefutása után automatikusan felszabadítja az objektumot. Átalakítottam a feltételes szerkezet logikáját. Ugyanúgy működik, mint eddig, de most már akkor is a "Username and Password are required!" szöveget jeleníti meg, ha valamelyik hiányos. A hibaüzenet megjelenésekor hangjelzés hallatszódik, ezt a "System.Media" névtér SystemSounds osztályával lehet megoldani.

--- projektFeladat_WPF/Views/LogIn.xaml.cs
+++ projektFeladat_WPF/Views/LogIn.xaml.cs
@@ -15,6 +15,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Media;
 
 namespace projektFeladat_WPF
 {
@@ -30,30 +31,62 @@ namespace projektFeladat_WPF
 
         private void LogInButton_Click(object sender, RoutedEventArgs e)
         {
-            if (textBoxEduId.Text!="" || passwordBox1.Password!="")
-            {
-                EducationDatabaseEntities ent = new EducationDatabaseEntities();
-                foreach (var item in ent.Users)
-                {
-                    if (textBoxEduId.Text==item.EduId && passwordBox1.Password==item.Password)
-                    {
-                        //MessageBox.Show("LoggedIn as: "+ item.FirstName +" password:"+item.Password +" place of birth: "+ item.BirthPlace); //TODO: kezelni, hogy milyen jogosultsággal lépett be
-                        testWindow testWindow = new testWindow();
-                        testWindow.Show();
-                        return;
-                    }
+            //if (textBoxEduId.Text!="" || passwordBox1.Password!="")
+            //{
+            //    EducationDatabaseEntities ent = new EducationDatabaseEntities();
+            //    foreach (var item in ent.Users)
+            //    {
+            //        if (textBoxEduId.Text==item.EduId && passwordBox1.Password==item.Password)
+            //        {
+            //            //MessageBox.Show("LoggedIn as: "+ item.FirstName +" password:"+item.Password +" place of birth: "+ item.BirthPlace); //TODO: kezelni, hogy milyen jogosultsággal lépett be
+            //            testWindow testWindow = new testWindow();
+            //            testWindow.Show();
+            //            return;
+            //        }
 
-                    //else
-                    //{
-                    //    errormessage.Text = " Invalid username or password!";
-                    //}
-                }
-                errormessage.Text = " Invalid username or password!";
+            //        //else
+            //        //{
+            //        //    errormessage.Text = " Invalid username or password!";
+            //        //}
+            //    }
+            //    errormessage.Text = " Invalid username or password!";
+            //    SystemSounds.Beep.Play();
+            //}
+            //else
+            //{
+            //    errormessage.Text = " Username and Password are required!";
+            //    SystemSounds.Beep.Play();
+
+            //}
+
+            if (textBoxEduId.Text == String.Empty || passwordBox1.Password == String.Empty)
+            {
+                errormessage.Text = " Username and Password are required!";
+                SystemSounds.Beep.Play();
             }
             else
             {
-                errormessage.Text = " Username and Password are required!";
-                    
+                using (EducationDatabaseEntities ent = new EducationDatabaseEntities())
+                {
+                    foreach (var user in ent.Users)
+                    {
+                        if (textBoxEduId.Text == user.EduId && passwordBox1.Password == user.Password)
+                        {
+                            //MessageBox.Show("LoggedIn as: "+ item.FirstName +" password:"+item.Password +" place of birth: "+ item.BirthPlace); //TODO: kezelni, hogy milyen jogosultsággal lépett be
+                            testWindow testWindow = new testWindow();
+                            testWindow.Show();
+                            return;
+                        }
+
+                        //else
+                        //{
+                        //    errormessage.Text = " Invalid username or password!";
+                        //}
+                    }
+                }
+                errormessage.Text = " Invalid username or password!";
+                SystemSounds.Beep.Play();
+
             }
         }
 
