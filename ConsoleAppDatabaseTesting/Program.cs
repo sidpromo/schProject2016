6988e60142e23054fa1eee494f8e0f37f6987c40
gravitydrummer <gravitydrummer@gmail.com>
2016-08-28 17:27:37 +0200

    Néhány adatbázisművelet kipróbálása.

--- ConsoleAppDatabaseTesting/Program.cs
+++ ConsoleAppDatabaseTesting/Program.cs
@@ -10,6 +10,55 @@ namespace ConsoleAppDatabaseTesting
     {
         static void Main(string[] args)
         {
+            EducationDatabaseEntities ent = new EducationDatabaseEntities();
+
+            User u = new User()
+            {
+                EduId = "52AF28",
+                Password = "testPassword",
+                Active = true,
+                UserType = "Student",
+                FirstName = "Peter",
+                MiddleName = null,
+                LastName = "Smith",
+                MothersName = "Julie Croeger",
+                BirthDate = new DateTime(1987, 4, 12),
+                BirthPlace = "Paris",
+                Nationality = "hungarian",
+                PersonalId = "82234234CA",
+                MedicalCardId = "223181053",
+                SemesterId = null,
+                SemesterState = "active",
+                EduCardId = "2344234BB"
+            };
+
+            User u2 = new User()
+            {
+                InsertDate = DateTime.Now,
+                EduId = "72BA11",
+                Password = "testPassword",
+                Active = true,
+                UserType = "Teacher",
+                FirstName = "Robert",
+                MiddleName = null,
+                LastName = "Bay",
+                MothersName = "Kate Popps",
+                BirthDate = new DateTime(1958, 7, 9),
+                BirthPlace = "Budapest",
+                Nationality = "hungarian",
+                PersonalId = "62234234CA",
+                MedicalCardId = "224185053",
+                SemesterId = null,
+                SemesterState = "active",
+                EduCardId = "3844234DC"
+            };
+
+            //ent.Users.Add(u2);
+
+            ent.Users.Remove(ent.Users.First(x => x.Id == 2));
+            ent.Users.Remove(ent.Users.First(x => x.Id == 3));
+
+            ent.SaveChanges();
         }
     }
 }
