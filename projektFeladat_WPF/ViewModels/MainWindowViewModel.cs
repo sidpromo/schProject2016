289d266626c1aa3e3aa055de903b287705e1d5e5
Bernat <Bernat@b>
2016-08-31 18:55:57 +0200

    Personal Infromation tab+ MainWindowViewModel

---
+++ projektFeladat_WPF/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,32 @@
+﻿using Repository;
+using Repository.UserRepos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projektFeladat_WPF.ViewModels
+{
+   public class MainWindowViewModel :  Bindable
+    {
+        static EducationDatabaseEntities ent = new EducationDatabaseEntities();
+        static UsersRepository userRepo = new UsersRepository(ent);
+
+        public string CurrentUserInfo { get; private set; }
+        private Users currentUser;
+
+        public Users CurrentUser
+        {
+            get { return currentUser; }
+            set { currentUser = value; }
+        }
+        public MainWindowViewModel(int id)
+        {
+            CurrentUser = userRepo.GetById(id);
+            CurrentUserInfo = String.Format("{0} {1} {2} ({3})", CurrentUser.FirstName, CurrentUser.MiddleName, CurrentUser.LastName, CurrentUser.EduId);
+           
+
+        }
+    }
+}
