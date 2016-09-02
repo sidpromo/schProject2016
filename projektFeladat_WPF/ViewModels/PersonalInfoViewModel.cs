54f4f5688aaa2026bcd8cb0bc87d2f07c5c29490
Bernat <Bernat@b>
2016-09-02 21:43:00 +0200

    PersonalInfoUserControl létrehozása

---
+++ projektFeladat_WPF/ViewModels/PersonalInfoViewModel.cs
@@ -0,0 +1,29 @@
+﻿using Entities;
+using projektFeladat_WPF.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WcfServiceLibrary;
+
+namespace projektFeladat_WPF.ViewModels
+{
+    class PersonalInfoViewModel
+    {
+        IService _service = new Service();
+
+        public string CurrentUserInfo { get; private set; }
+        private Users currentUser;
+
+        public Users CurrentUser
+        {
+            get { return currentUser; }
+            set { currentUser = value; }
+        }
+        public PersonalInfoViewModel()
+        {
+            CurrentUser = _service.GetUserById((int)Singleton.Instance.ID);
+        }
+    }
+}
