5c4a894050ed7d4f26cac0b22fe28a7622810bb9
Bernat <yoyoklip@hotmail.com>
2016-09-10 20:10:35 +0200

    felesleges using-ok kiszedve

--- projektFeladat_WPF/Converters/SubjectIdToSubjectNameConverter.cs
+++ projektFeladat_WPF/Converters/SubjectIdToSubjectNameConverter.cs
@@ -1,12 +1,7 @@
 ﻿using projektFeladat_WPF.NeptunServiceReference;
 using System;
-using System.Collections.Generic;
 using System.Globalization;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Data;
-using WcfServiceLibrary;
 
 namespace projektFeladat_WPF.Converters
 {
